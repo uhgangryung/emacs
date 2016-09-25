cee189914edb97b6500300e59bce5f1d5edd2a76
uhgangryung <gangryunguh@gmail.com>
2016-09-25 09:23:45 -0500

    Window mode (ace-window)

--- init.el
+++ init.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (tabbar which-key 2048-game try use-package))))
+ '(package-selected-packages
+   (quote
+    (ace-window tabbar which-key 2048-game try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -53,3 +55,13 @@
   :ensure t
   :config
   (tabbar-mode 1))
+
+(use-package ace-window
+  :ensure t
+  :init
+  (progn
+    (global-set-key [remap other-window] 'ace-window)
+    (custom-set-faces
+     '(aw-leading-char-face
+       ((t (:inherit ace-jump-fae-foreground :height 3.0)))))
+    ))
