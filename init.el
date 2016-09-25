956556eb4b0d670f788f72831c63c49b4ea3b83e
uhgangryung <gangryunguh@gmail.com>
2016-09-25 09:06:43 -0500

    init.el with buffer

---
+++ init.el
@@ -0,0 +1,55 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (tabbar which-key 2048-game try use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Our stuff
+(setq inhibit-startup-message t)
+(tool-bar-mode -1)
+
+(require 'package)
+(setq package-enable-at-stasrtup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package try
+  :ensure t)
+
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
+
+;; Org-mode stuff
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+(setq indo-enable-flex-matching t)
+
+(setq ido-everyewhere t)
+(ido-mode 1)
+
+(defalias 'list-buffers 'ibuffer)
+;; (defalias 'list-buffers 'ibuffer-other-window)
+
+
+; If you like a tabbar
+(use-package tabbar
+  :ensure t
+  :config
+  (tabbar-mode 1))
