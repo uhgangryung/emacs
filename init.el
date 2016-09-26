341278f0598b68e2ea12f3160c94e8247a4a9494
uhgangryung <gangryunguh@gmail.com>
2016-09-25 22:28:59 -0500

    Clean-up init.el with myinit.org

--- init.el
+++ init.el
@@ -1,107 +1,19 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (avy-flycheck ace-window tabbar which-key 2048-game try use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-fae-foreground :height 3.0)))))
-
-;; Our stuff
-(setq inhibit-startup-message t)
-(tool-bar-mode -1)
-
 (require 'package)
 (setq package-enable-at-stasrtup nil)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
-
 (package-initialize)
 
+;; Bootstrap 'use-package'
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-(use-package try
-  :ensure t)
-
-(use-package which-key
-  :ensure t
-  :config (which-key-mode))
-
-;; Org-mode stuff
-(use-package org-bullets
-  :ensure t
-  :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
-
-(setq indo-enable-flex-matching t)
-
-(setq ido-everyewhere t)
-(ido-mode 1)
-
-(defalias 'list-buffers 'ibuffer)
-;; (defalias 'list-buffers 'ibuffer-other-window)
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 
 
-; If you like a tabbar
-(use-package tabbar
-  :ensure t
-  :config
-  (tabbar-mode 1))
-
-(use-package ace-window
-  :ensure t
-  :init
-  (progn
-    (global-set-key [remap other-window] 'ace-window)
-    (custom-set-faces
-     '(aw-leading-char-face
-       ((t (:inherit ace-jump-fae-foreground :height 3.0)))))
-    ))
-
-(use-package ivy
-  :ensure t
-  :diminish (ivy-mode)
-  :bind (("C-x b" . ivy-switch-buffer))
-  :config
-  (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t)
-  (setq ivy-display-style 'fancy))
-  
-;;
-;; search with swiper - precondition counsel package needs to be installed
-;;
-;;(use-package counsel
-;;  :ensure t
-;;  )
-(use-package swiper
-  :ensure try
-  :bind (("C-s" . swiper)
-	 ("C-r" . swiper)
-	 ("C-c C-r" . ivy-resume)
-	 ("M-x" . counsel-M-x)
-	 ("C-x C-f" . counsel-find-file))
-  :config
-  (progn
-    (ivy-mode 1)
-    (setq ivy-use-virtual-buffers t)
-    (setq ivy-display-style 'fancy)
-    (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
-    ))
 
 
-;; 
-;; Search with avy - not sure of the power of search ...
 
-(use-package avy
-  :ensure t
-  :bind ("M-s" . avy-goto-char))
 
 
