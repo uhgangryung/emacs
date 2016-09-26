67ed201c5b22988e62bb9b0333080907ab323779
uhgangryung <gangryunguh@gmail.com>
2016-09-26 05:32:56 -0500

    javascript jreveal package for quick html presentation demo

--- init.el
+++ init.el
@@ -17,3 +17,17 @@
 
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (htmlize ox-reveal zenburn-theme which-key use-package try tabbar org-bullets evil-org elfeed-org counsel color-theme avy-flycheck auto-complete ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-fae-foreground :height 3.0)))))
