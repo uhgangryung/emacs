b77569a05c2d9eedf6f1fd90cdbf4a6ae7aaf981
Gang-Ryung <uh@ubuntu>
2016-09-26 17:41:46 -0700

    yasnippet and flask

--- samples/python/test.py
+++ samples/python/test.py
@@ -6,3 +6,22 @@ def f(n):
 for i in range(1,10):
     print random.randrange(2,10)
 
+    
+if __name__ == '__main__':
+    main()
+
+
+class myclass(object):
+    """Documentation for myclass
+
+    """
+    def __init__(self, some_args):
+        super(myclass, self).__init__()
+        self.some_args = some_args
+        
+        
+        
+
+        
+            
+
